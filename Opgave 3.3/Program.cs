aa27186950c502098673118a3d7623ecfee0fe82
Christoffer Olsen <chris70944@gmail.com>
2024-02-05 11:59:08 +0100

    Færdig

--- Opgave 3.3/Program.cs
+++ Opgave 3.3/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var pizza = Console.ReadLine();
+Console.Clear();
+Console.WriteLine($"Du synes at {pizza} er den bedste pizza i verden.");
